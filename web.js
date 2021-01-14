a75df7251b4c0137cdcf8bd912f48aa1a0110d35
= <=>
2021-01-14 13:35:14 +0900

    dsa

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 8006;
 // buan
 const buanIndexRouter = require('./routers/buan/index');
 const buanAboutRouter = require('./routers/buan/about');
