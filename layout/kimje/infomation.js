ab037217b45fa8b1d1d3e84768bab394154886e8
= <=>
2021-01-28 18:00:31 +0900

    das

---
+++ layout/kimje/infomation.js
@@ -0,0 +1,61 @@
+exports.content = ()=>{
+    var template = `
+    <main class="main_container">
+        <div class="attech_bg">
+            <h3>Infomation</h3>
+        </div>
+        <article class="one_content_box">
+        <h3>공지사항</h3>
+          <table>
+            <thead>
+                <tr>
+                    <td>번호</td>
+                    <td>제목</td>
+                    <td>작성자</td>
+                    <td>등록일</td>
+                    <td>조회수</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>[지금김제펜션] 오픈 할인 행사</td>
+                    <td>관리자</td>
+                    <td>2021-01-28</td>
+                    <td>430</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>[지금김제펜션] 오픈 할인 행사</td>
+                    <td>관리자</td>
+                    <td>2021-01-28</td>
+                    <td>430</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>[지금김제펜션] 오픈 할인 행사</td>
+                    <td>관리자</td>
+                    <td>2021-01-28</td>
+                    <td>430</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>[지금김제펜션] 오픈 할인 행사</td>
+                    <td>관리자</td>
+                    <td>2021-01-28</td>
+                    <td>430</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>[지금김제펜션] 오픈 할인 행사</td>
+                    <td>관리자</td>
+                    <td>2021-01-28</td>
+                    <td>430</td>
+                </tr>
+            </tbody>
+          </table>
+        </article>
+    </main>
+    `
+    return template;
+}
---
+++ layout/kimje/travel.js
@@ -0,0 +1,46 @@
+exports.content = ()=>{
+    var template = `
+    <main class="main_container">
+        <div class="attech_bg">
+            <h3>Travel</h3>
+        </div>
+        <article class="one_content_box">
+            <ul>
+                <a href="/kimje/travel">
+                    <li>
+                        <img src="../public/kimje/img/travel/travel_1.png" alt="방 썸네일"/>
+                        <div class="room_black">
+                            <p>김제 망해사</p>
+                        </div>
+                    </li>
+                </a>
+                <a href="/kimje/travel">
+                    <li>
+                        <img src="../public/kimje/img/travel/travel_2.png" alt="방 썸네일"/>
+                        <div class="room_black">
+                            <p>김제 금산사</p>
+                        </div>
+                    </li>
+                </a>
+                <a href="/kimje/travel">
+                    <li>
+                        <img src="../public/kimje/img/travel/travel_3.png" alt="방 썸네일"/>
+                        <div class="room_black">
+                            <p>김제 벽골제</p>
+                        </div>
+                    </li>
+                </a>
+                <a href="/kimje/travel">
+                    <li>
+                        <img src="../public/kimje/img/travel/travel_4.png" alt="방 썸네일"/>
+                        <div class="room_black">
+                            <p>김제 모악산</p>
+                        </div>
+                    </li>
+                </a>
+            </ul>
+        </article>
+    </main>
+    `
+    return template;
+}
